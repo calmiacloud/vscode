2b0571b20fccb8652c225467fcd8a1fd4e912d41
calmiacloud <calmiacloud@gmail.com>
2025-09-17 03:17:34 +0200

    Update postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,1 +1,3 @@
-  "postCreateCommand": "chmod 600 ~/.ssh/id_ed25519 && ssh-keyscan github.com >> ~/.ssh/known_hosts"
+rm -rf /workspaces/vscode/.git 
+chmod 600 ~/.ssh/id_ed25519 && ssh-keyscan github.com >> ~/.ssh/known_hosts
+echo 'Host github.com\n  IdentityFile ~/.ssh/id_ed25519\n  User git' >> ~/.ssh/config && chmod 600 ~/.ssh/config
